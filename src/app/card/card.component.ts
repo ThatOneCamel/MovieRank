012d0edf3ea446a17b90e75985360cb225954eb3
TCameronT <thomascameront@gmail.com>
2020-06-28 20:27:58 -0500

    Added CSS for Card Component + Updated images to be a fixed size + Made text a fixed size and applied Open Sans font - TODO: Make text size dynamic + Fixed element size issues when text was too long + Text is now truncated after 2 lines

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -10,6 +10,10 @@ export class CardComponent implements OnInit {
   @Input() title: string;
   @Input() srcStr: string;
 
+  onClick(): void {
+    console.log("Hey you clicked a card")
+  }
+
   constructor() { }
 
   ngOnInit(): void {
