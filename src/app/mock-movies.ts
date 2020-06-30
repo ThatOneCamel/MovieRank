8fd85bb84d5320e3b273bbf29d20d17084f3af69
TCameronT <thomascameront@gmail.com>
2020-06-29 19:20:43 -0500

    Implemented mock data and movie object template

---
+++ src/app/mock-movies.ts
@@ -0,0 +1,28 @@
+import { Movie } from './movie';
+
+export const MOVIES: Movie[] = [
+    { id: 10, title: "Cabin in the Woods"},
+    { id: 11, title: "Back to the Future" },
+    { id: 12, title: "Mission Impossible Fallout" },
+    { id: 13, title: "Inception" },
+    { id: 14, title: "Baby Driver" },
+    { id: 15, title: "Frances Ha" },
+    { id: 16, title: "Waves" },
+    { id: 17, title: "Silence of the Lambs" },
+    { id: 18, title: "Spider-Man 2" },
+    { id: 19, title: "Harry Potter: Half Blood Prince" },
+    { id: 20, title: "Inglorious Basterds" },
+    { id: 21, title: "Alien" },
+    { id: 22, title: "La La Land" },
+    { id: 23, title: "Super 8" },
+    { id: 24, title: "Empire Strikes Back" },
+    { id: 25, title: "Lilo & Stitch" },
+    { id: 26, title: "Hereditary" },
+    { id: 27, title: "Toy Story" },
+    { id: 28, title: "Scream" },
+    { id: 29, title: "The Witch" },
+    { id: 30, title: "E.T. The Extra-Terrestrial" },
+
+
+];
+
---
+++ src/app/movie.ts
@@ -0,0 +1,10 @@
+import { Url } from 'url';
+
+export interface Movie {
+    id: number;
+    title: string;
+    year?: number;
+    release?: Date;
+    poster?: Url;
+
+}
