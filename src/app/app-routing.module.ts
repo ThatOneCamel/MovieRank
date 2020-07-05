2e6a5e1e4125aeadc29bcbff676833bbd8dbdfe4
TCameronT <thomascameront@gmail.com>
2020-07-05 17:40:37 -0500

    Separated elements into components + Header is now its own component + Created input component - Made CSS for header button universal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from '../app/header/header.component'
+import { InputComponent } from '../app/input/input.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'makelist', component: InputComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { SelectorBtnComponent } from './selector-btn/selector-btn.component';
+import { HeaderComponent } from './header/header.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    SelectorBtnComponent
+    SelectorBtnComponent,
+    HeaderComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
