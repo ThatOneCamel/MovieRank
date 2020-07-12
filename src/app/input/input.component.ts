f0a9d0dd0e1be720c929621899e157e2df7d33e3
TCameronT <thomascameront@gmail.com>
2020-07-11 20:05:57 -0500

    Changed input component's list to type 'Movie'

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MovieListService } from '../movie-list.service';
 import { Movie } from '../movie';
+import * as MovieManager from '../rating/movie-manager';
 
 @Component({
   selector: 'app-input',
@@ -10,25 +11,32 @@ import { Movie } from '../movie';
 export class InputComponent implements OnInit {
 
   @Input() item: string = '';
-  list: string[] = [];
+  list: Movie[] = [];
   counter: number = 0;
 
   constructor() { }
 
   onEnter(val: string): void {
-    this.list.push(val);
-    console.log(this.list);
+    this.list.push({id: this.counter, title: val});
+    this.counter++;
     this.clear();
+  }
+
+  displayList(): string {
+    return MovieManager.getMovieTitles(this.list).join(' | ');
   }
 
   passList(): void {
     console.log("List contained:");
     console.log(this.list);
-    //this.movieService.movies = this.list;
   }
 
   clear(): void {
     this.item = '';
+  }
+
+  clearList(): void {
+    this.list = [];
   }
 
   ngOnInit(): void {
